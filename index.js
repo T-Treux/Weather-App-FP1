b9aa3198d7e8df40b5ec3c84365ed444f950b2b0
T-Treux <110358968+T-Treux@users.noreply.github.com>
2022-09-18 19:19:15 -0500

    modified:   index.html 	modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML =
+  
+}
+
+let apiKey = "2470c77565feed5f5dce3347bf4cb0f2";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={Park Forest, IL}&appid=${apiKey}";
+  "https://api.openweathermap.org/data/2.5/weather?q={Chicago}&appid={2470c77565feed5f5dce3347bf4cb0f2}&units=metrics";
+
+axios.get(apiUrl).then(displayTemperature);
