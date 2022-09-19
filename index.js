3e2a9f9c89afdb6509ba8d740b9e9f3d4c1193b6
T-Treux <110358968+T-Treux@users.noreply.github.com>
2022-09-18 20:45:43 -0500

    modified:   index.html 	modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  description.innerHTML =
-  
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "2470c77565feed5f5dce3347bf4cb0f2";
